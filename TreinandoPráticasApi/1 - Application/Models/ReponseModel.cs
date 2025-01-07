120d63e8ad8d484dadf87688b8e412a4543aa2f9
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2025-01-06 21:59:32 -0300

    feat : Criando Models para o token

---
+++ TreinandoPráticasApi/1 - Application/Models/ReponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TreinandoPráticasApi._1___Application.Models
+{
+    public class ReponseModel
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
